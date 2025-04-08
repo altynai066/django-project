0d5da89424bb25ecceb871768480d8933b4f1965
Mahabbat07 <toktarmahabbat507@gmail.com>
2025-04-09 01:12:02 +0500

    Добавлена страница регистрации, настроены URL и формы

---
+++ myapp/admin.py
@@ -0,0 +1,12 @@
+# myapp/admin.py
+from django.contrib import admin
+from .models import Task
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')  # Поля, которые будут отображаться в списке
+    search_fields = ('title',)  # Поиск по названию задачи
+    list_filter = ('created_at',)  # Фильтр по дате создания
+from django.contrib import admin
+
+# Register your models here.
