80b4b9475392cce8dbfc1a340885a1ae9e82d9f0
Mahabbat07 <toktarmahabbat507@gmail.com>
2025-04-09 11:41:40 +0500

    Были внесены изменения

--- myapp/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Функция для главной страницы
-def index(request):
-    return render(request, 'index.html')  # Здесь предполагается, что у вас есть шаблон index.html
